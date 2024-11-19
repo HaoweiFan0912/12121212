00c0368f0bcc0d32f69ac3c2863d2aabe731daa1
HaoweiFan0912 <fhw0912@gmail.com>
2024-11-19 17:07:57 -0500

    Initial commit

---
+++ 2323.R
@@ -0,0 +1,19 @@
+#### Preamble ####
+# Purpose: Models... [...UPDATE THIS...]
+# Author: Rohan Alexander [...UPDATE THIS...]
+# Date: 11 February 2023 [...UPDATE THIS...]
+# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# License: MIT
+# Pre-requisites: [...UPDATE THIS...]
+# Any other information needed? [...UPDATE THIS...]
+
+
+#### Workspace setup ####
+library(tidyverse)
+library(rstanarm)
+library(arrow)
+#### Read data ####
+analysis_data <- read_parquet("00-raw_data_combined.parquet")
+
+
+
